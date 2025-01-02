f41aad44a41516c732241155d1c43ac126ddd20e
Malee <N.Khanthamalee@gmail.com>
2025-01-02 17:53:40 +0700

    add crud

--- lib/gsheet_CRUD.dart
+++ lib/gsheet_CRUD.dart
@@ -1,6 +1,16 @@
 import 'gsheet_setup.dart';
 
-InsertDataIntoGSheet() async {
-  await GsheetCRUDUserDetails!.values.map.appendRows(maps);
+void InsertDataIntoGSheet(data) async {
+  await GsheetCRUDUserDetails!.values.map.appendRows(data);
   print("Data stored");
+}
+
+List DataFromGsheet = [];
+
+void readDatafromGSheet() async {
+  DataFromGsheet = (await GsheetCRUDUserDetails!.values.map.allRows())!;
+  print("Data Fetched");
+  print("DataFromGsheet : ${DataFromGsheet[0]["Date"].runtimeType}");
+  print(
+      "DataFromGsheet : ${DateTime.fromMicrosecondsSinceEpoch(double.parse(DataFromGsheet[0]["Date"]).toInt())}");
 }
